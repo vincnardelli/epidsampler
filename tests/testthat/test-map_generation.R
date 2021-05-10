4c5ff54ac8aff3e740d599b00dab85121021bb4c
Niccolò Salvini <niccolo.salvini27@gmail.com>
2021-05-11 01:44:27 +0200

    updated tests

--- tests/testthat/test-map_generation.R
+++ tests/testthat/test-map_generation.R
@@ -1,5 +1,5 @@
 set.seed(1234)
-map <- generate(n=4, P=100, type="grid")
+map <- generate(n = 4, P = 100, type = "grid")
 
 context("Map generation")
 
@@ -8,28 +8,37 @@ test_that("map generation parameters", {
 })
 
 test_that("map generation contact all zero", {
-  expect_equal(0,
-               sum(unlist(lapply(map$contacts, function(x) length(x)))))
+  expect_equal(
+    0,
+    sum(unlist(lapply(map$contacts, function(x) length(x))))
+  )
 })
 
 test_that("map generation starting exposed people", {
-  expect_equal(sum(map$data$condition == "E"),
-               10)
+  expect_equal(
+    sum(map$data$condition == "E"),
+    10
+  )
 })
 
 test_that("map generation print", {
-  expect_equal(capture.output(print(map)),
-               c("Epidmic generated map ", "Dimension 2 x 2 ", "Total 100 people"))
+  expect_equal(
+    capture.output(print(map)),
+    c("Epidmic generated map ", "Dimension 2 x 2 ", "Total 100 people")
+  )
 })
 
 test_that("map generation plot", {
-  expect_error(plot(map),
-               "Not enough data")
+  expect_error(
+    plot(map),
+    "Not enough data"
+  )
 })
 
-map <- generate(n=4, P=100, save_movements = TRUE)
+map <- generate(n = 4, P = 100, save_movements = TRUE)
 test_that("map generation verbose", {
-  expect_equal(map$movements,
-               NULL)
+  expect_equal(
+    map$movements,
+    NULL
+  )
 })
-
