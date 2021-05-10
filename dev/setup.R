a81765ff50d69cb81abaf04a59feccf911b16c64
Niccolò Salvini <niccolo.salvini27@gmail.com>
2021-05-11 01:02:53 +0200

    added desc changes to setup.R

--- dev/setup.R
+++ dev/setup.R
@@ -6,10 +6,10 @@ usethis::use_description(
   list(
     Title = "Simulations for Sampling in Epidemics",
     `Authors@R` = "c(
-    person('Vincenzo', 'Nardelli', email = 'vincnardelli@gmail.com', role = c('cre', 'aut')),
-    person('Giorgio', 'Alleva', role = c('ctb')),
+    person('Vincenzo', 'Nardelli', email = 'vincnardelli@gmail.com', role = c('cre', 'aut', 'cph'), comment = c(ORCID = '0000-0002-7215-7934')),
+    person('Giorgio', 'Alleva', role = c('ctb'), comment = c(ORCID = '0000-0002-3910-3029')),
     person('Giuseppe', 'Arbia', email = 'giuseppearbia13@gmail.com', role = c('ctb')),
-    person('Piero Demetrio', 'Falorsi', role = c('ctb')),
+    person('Piero Demetrio', 'Falorsi', role = c('ctb'), comment = c(ORCID = '0000-0001-5173-3931')),
     person('Alberto', 'Zuliani', role = c('ctb'))
     )",
     Description = "Simulate an epidemic map with mobility and social interaction between individuals. Useful for testing sampling methods.",
@@ -21,19 +21,22 @@ usethis::use_tidy_description()
 
 usethis::use_package("magrittr")
 usethis::use_pipe(export = TRUE)
-usethis::use_package("dplyr")
-usethis::use_package("sf")
-usethis::use_package("ggplot2")
+usethis::use_package("dplyr", min_version = T)
+usethis::use_package("sf", min_version = T)
+usethis::use_package("ggplot2", min_version = T)
 usethis::use_package("spdep")
-usethis::use_package("purrr")
+usethis::use_package("purrr", min_version = T)
 usethis::use_package("parallel")
 usethis::use_package("progress")
-usethis::use_package("gganimate")
-usethis::use_package("testthat", "Suggest")
+usethis::use_package("gganimate", min_version = T)
+usethis::use_package("testthat", "Suggest", min_version = T)
 usethis::use_package("utils", "Suggest")
 usethis::use_package("methods")
 usethis::use_package("stats")
 usethis::use_vignette(name="epidsampler")
+
+# URL and bugReports
+use_github_links(overwrite = T)
 
 
 # Read me
